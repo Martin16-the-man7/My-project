91dfb5701fbcfc0801dcf0a88e964cba41ff3b50
Martin16-the-man7 <martin.hymas@malad.us>
2026-09-08 12:12:55 -0600

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/**
+ * Hello
+ */
+public class Hello {
+ public static void main(String[] args) {
+    String name = "I am martin and 16 years old";
+    String Dad = "My dad is named Reed and is 50 years old";
+    String Mom = "My mom is named Kristine and is 48 years old";
+    String sister = "My sister is named Tiffany and is 12 years old";
+    String Bethany = "My sister is named Bethany and is 18 years old";
+    String brother = "My brother is named Zachary and is 11 years old";
+   
+        System.out.println("Name: " + name);
+        System.out.println("Dad: " + Dad);
+        System.out.println("Mom: " + Mom);
+        System.out.println("sister: " + sister);
+        System.out.println("Bethany: " + Bethany);
+        System.out.println("Brother: " + brother);
+    }
+}
